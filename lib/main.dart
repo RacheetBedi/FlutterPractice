71d12c77ed695074b7111cfc9bc573236a42dd39
Racheet_Bedi <racheetbedi25@gmail.com>
2025-09-01 22:37:32 +0000

    Added MyApp class and StatelessWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text(
-    'Hello, World!!!',
-    textDirection: TextDirection.rtl,
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Hello, World!!!',
+      textDirection: TextDirection.rtl,
+    );
+  }
 }
