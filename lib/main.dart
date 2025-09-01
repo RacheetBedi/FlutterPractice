efff441b7901369d507c25f51559f57f484720c8
Racheet_Bedi <racheetbedi25@gmail.com>
2025-09-01 22:50:17 +0000

    Understanding Scaffold, Center, and the Widget Tree.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Hello, World!!!',
-      textDirection: TextDirection.rtl,
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!!!!'),
+        ),
+      ),
     );
   }
 }
